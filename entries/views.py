4f9d4d5a90f5a4f00260d2228830e65a32eb1d61
Rajesh Moka <rajeshmoka@icloud.com>
2020-05-22 20:29:43 +0530

    added apps and templates

---
+++ entries/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Entry
+
+class HomeView(ListView):
+	model = Entry
+	template_name = 'entries/index.html'
+	context_object_name = 'blog_entries'
+	ordering = ['-entry_date']
+	paginate_by = 3
+
+class EntryView(DetailView):
+	model = Entry
+	template_name = 'entries/entryDetail.html'
+
+class CreateEntryView(LoginRequiredMixin,CreateView):
+	model = Entry
+	template_name = 'entries/createEntry.html'
+	fields = ['entry_title','entry_text']
+
+	def form_valid(self, form):
+		form.instance.entry_author = self.request.user
+		return super().form_valid(form)
